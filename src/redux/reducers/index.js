f93d0074c526236d73f4abb4782dad80f27c419b
muhammadsaad25 <muhammadsaad2522@gmail.com>
2024-07-05 17:55:34 +0500

    remove code

--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import currencyReducer from "./currencyReducer";
-import authReducer from "./authReducer";
-const rootReducer = combineReducers({
-  currencyReducer: currencyReducer,
-  authReducer: authReducer,
-});
-export default rootReducer;
